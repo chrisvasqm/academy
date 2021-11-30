8d2ff6fa4585a0cff12613fa8499b43d8df056e0
chrisvasqm <christianvasquezmelo@gmail.com>
2021-11-29 20:59:55 -0400

    Add total price per Subject

--- academy/serializers.py
+++ academy/serializers.py
@@ -1,9 +1,14 @@
+from re import sub
 from rest_framework import serializers
 
 from .models import Subject
 
 class SubjectSerializer(serializers.ModelSerializer):
+    total_price = serializers.SerializerMethodField(method_name='get_total_price')
     
     class Meta:
         model = Subject
-        fields = ['id', 'credits', 'price_per_credit', 'academy']
+        fields = ['id', 'credits', 'price_per_credit', 'academy', 'total_price']
+        
+    def get_total_price(self, subject: Subject):
+        return subject.credits * subject.price_per_credit
