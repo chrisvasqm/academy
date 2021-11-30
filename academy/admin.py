ebbe6796ef4277ca52520cfe62c6667c4f8cb973
chrisvasqm <christianvasquezmelo@gmail.com>
2021-11-30 10:07:44 -0400

    Add Subject admin area

--- academy/admin.py
+++ academy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from academy.models import Subject
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'credits', 'price_per_credit']
+    list_editable = ['credits', 'price_per_credit']
+    list_per_page = 10
+    search_fields = ['name']
