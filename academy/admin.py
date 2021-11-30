f10029da9567ad75870a7de28ea6b2105c43745c
chrisvasqm <christianvasquezmelo@gmail.com>
2021-11-30 10:17:14 -0400

    Add Academy admin module

--- academy/admin.py
+++ academy/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from academy.models import Subject
+from academy.models import Academy, Subject
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ['name', 'credits', 'price_per_credit', 'academy']
     list_editable = ['credits', 'price_per_credit', 'academy']
     list_per_page = 10
+    search_fields = ['name']
+
+@admin.register(Academy)
+class AcademyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
     search_fields = ['name']
