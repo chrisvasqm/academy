5eb360c88d606bc3051286063e6f9c95cab2969f
chrisvasqm <christianvasquezmelo@gmail.com>
2021-11-30 10:11:33 -0400

    Override toString methods

--- academy/models.py
+++ academy/models.py
@@ -14,11 +14,17 @@ class Subject(models.Model):
     price_per_credit = models.DecimalField(max_digits=6, decimal_places=2)
     academy = models.ForeignKey(Academy, on_delete=CASCADE, related_name='academies')
     
+    def __str__(self) -> str:
+        return self.name
+    
 class Student(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
     phone = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.first_name + ' ' + self.last_name
     
 class Enrollment(models.Model):
     subject = models.ForeignKey(Subject, on_delete=PROTECT, related_name='subjects')
